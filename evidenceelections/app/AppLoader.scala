f1877f66d299d5384b38c857164bcf914972f525
bhglassman <bhglassman@gmail.com>
2018-06-09 09:45:21 -0400

    Actors section of tutorial

--- evidenceelections/app/AppLoader.scala
+++ evidenceelections/app/AppLoader.scala
@@ -7,13 +7,21 @@ import router.Routes
 import play.api.routing.Router
 import com.softwaremill.macwire._
 import _root_.controllers.AssetsComponents
+import actors.StatsActor
+import actors.StatsActor.Ping
+import akka.actor.Props
 import play.filters.HttpFiltersComponents
+
+import scala.concurrent.Future
 import services.{SunService, WeatherService}
+import filters.StatsFilter
+
 class AppApplicationLoader extends ApplicationLoader { def load(context: Context) = {
   LoggerConfigurator(context.environment.classLoader).foreach { cfg => cfg.configure(context.environment)
   }
   new AppComponents(context).application }
 }
+
 class AppComponents(context: Context) extends
   BuiltInComponentsFromContext(context) with AhcWSComponents
   with AssetsComponents with HttpFiltersComponents {
@@ -23,5 +31,21 @@ class AppComponents(context: Context) extends
   lazy val applicationController = wire[Application]
   lazy val sunService = wire[SunService]
   lazy val weatherService = wire[WeatherService]
+  applicationLifecycle.addStopHook { () =>
+    Logger.info("The app is about to stop")
+    Future.successful(Unit)
   }
+
+  val onStart = {
+    Logger.info("The app is about to start")
+    statsActor ! Ping
+  }
+
+  lazy val statsFilter: Filter = wire[StatsFilter]
+  override lazy val httpFilters = Seq(statsFilter)
+
+  lazy val statsActor = actorSystem.actorOf(
+    Props(wire[StatsActor]), StatsActor.name)
+
+}
 
