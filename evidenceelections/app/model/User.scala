32e5d87ce96dad11e8bdcf79056e67025d2d7d30
bhglassman <bhglassman@gmail.com>
2018-06-10 11:28:50 -0400

    add db access method via companion object

--- evidenceelections/app/model/User.scala
+++ evidenceelections/app/model/User.scala
@@ -1,4 +1,11 @@
 package model
 
+import scalikejdbc.WrappedResultSet
+
 case class User(id: BigInt, user_name: String, password: String)
 
+object User {
+  def fromRS(rs: WrappedResultSet): User = {
+    User(rs.bigInt("id"),rs.string("user_name"),rs.string("password"))
+  }
+}
