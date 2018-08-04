b8eafec10fa809d2a13862b582e0fe80ba00c701
bhglassman <bhglassman@gmail.com>
2018-08-04 18:51:02 -0400

    added navbar

--- ui/scripts/TopNavbar.jsx
+++ ui/scripts/TopNavbar.jsx
@@ -21,7 +21,7 @@ class TopNavbar extends React.Component {
                             <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Profile</a>
+                            <a className="nav-link" href="/user">Profile</a>
                         </li>
                         <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
