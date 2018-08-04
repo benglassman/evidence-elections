a8c0faaabb91cfd1684da28551cc961432619061
zachglassman <zach.glassman@gmail.com>
2018-08-04 15:10:40 -0700

    alter evolutions to remove brackets

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -3,9 +3,9 @@
 # --- !Ups
 CREATE TABLE if not exists state(
 
-[stateID] [int] NOT NULL AUTO_INCREMENT,
-[stateCode] [nchar](2) NOT NULL,
-[stateName] [nvarchar](128) NOT NULL,
+stateID int NOT NULL AUTO_INCREMENT,
+stateCode nchar(2) NOT NULL,
+stateName nvarchar(128) NOT NULL,
 primary key(stateID)
 );
 
