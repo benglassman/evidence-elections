9922385bd57ada9a2258e481f5ee1dbc182bc825
benglassman <bhglassman@gmail.com>
2018-08-05 16:09:40 +0200

    Add frontend (#7)
    * add basic javascript boilerplate for webpack and yarn
    
    * add concise.css
    
    * switch to bootstrap because Concise Docs suck
    
    * added navbar
    
    * added navbar

--- ui/scripts/main.js
+++ ui/scripts/main.js
@@ -2,5 +2,7 @@ import Bootstrap from 'bootstrap'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IdentityComponent from './IdentityComponent.jsx';
+import TopNavbar from './TopNavbar.jsx';
 
+ReactDOM.render(<TopNavbar />, document.getElementById('navbarView'));
 ReactDOM.render(<IdentityComponent />, document.getElementById('reactView'));
